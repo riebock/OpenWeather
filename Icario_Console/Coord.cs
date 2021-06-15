b53dfead5b1be753696cbb076e46085eb1d7e991
riebock <85960447+riebock@users.noreply.github.com>
2021-06-15 13:12:56 -0500

    Initial Coce Commit

---
+++ Icario_Console/Coord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icario_Console
+{
+    public class Coord
+    {
+        public double lat { get; set; }
+        public double lon { get; set; }
+    }
+}
